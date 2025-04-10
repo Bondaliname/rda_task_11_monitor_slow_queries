26f5fdbb0f73f0f3ae1e30071866d8b40baf420e
Bondaliname <lina@Lenovo-Thinkpad.>
2025-04-10 10:20:20 +0000

    Solution

--- task.sql
+++ task.sql
@@ -10,6 +10,7 @@ CREATE TABLE Products1 (
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
+
 -- Create test data in Products1 table 
 INSERT INTO Products1 (Name)
 	VALUES ('AwersomeProduct1'); 
@@ -131,3 +132,6 @@ INSERT INTO Products1 (Name)
 	VALUES ('AwersomeProduct59'); 
 INSERT INTO Products1 (Name)
 	VALUES ('AwersomeProduct60'); 
+
+
+-- Create test data in Products2 table
